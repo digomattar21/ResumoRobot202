c94609fa0ece2b8c01d1d54c618fa5264b104f68
digomattar21 <digomattar@gmail.com>
2020-11-24 15:22:33 -0300

    .

--- Arquivos/Cv2/funcoes_importantes.py
+++ Arquivos/Cv2/funcoes_importantes.py
@@ -297,11 +297,11 @@ def find_extreme_points(image):
     return extRight, extBot, extLeft, extTop
 
 
-def find_box_points(image):
-    paper = image.copy()
-    ret, thresh_gray = cv2.threshold(cv2.cvtColor(paper, cv2.COLOR_BGR2GRAY),200, 255, cv2.THRESH_BINARY)
-    image, contours, hier = cv2.findContours(thresh_gray, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    for c in contours:
+def find_box_points(mask):
+   
+    contornos, arvore = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contornos:
         area = cv2.contourArea(c)
         rect = cv2.minAreaRect(c)
         box = cv2.boxPoints(rect)
@@ -309,10 +309,7 @@ def find_box_points(image):
         box = np.int0(box)
         # draw a green 'nghien' rectangle
         if area>500:
-            cv2.drawContours(paper, [box], 0, (0, 255, 0),1)
+            cv2.drawContours(mask, [box], 0, (0, 255, 0),1)
             print([box])
-
-    cv2.imshow('paper', paper)
-    cv2.waitKey(0)
 
 
