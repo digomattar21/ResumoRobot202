d82dc0197ed93111c6ab5ec1fd74c355d3512545
digomattar21 <digomattar@gmail.com>
2020-11-25 12:23:10 -0300

    .

--- Arquivos/Ros/funcoes_importantes.py
+++ Arquivos/Ros/funcoes_importantes.py
@@ -236,6 +236,84 @@ def auto_canny(image, sigma=0.5):
     return edged
 
 
+def conta_contornos(mask, title):
+    """Recebe uma imagem binaria e conta quantos contornos"""
+    contornos, arvore = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)     
+    desenha_contorno(mask, contornos, title)
+    return len(contornos)
+
+def desenha_contorno(mask, contornos, title): 
+    mask_rgb = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(mask_rgb, contornos, -1, [255, 0, 0], 5);
+    cv2.imshow(title, mask_rgb)
+
+def conta_pixels(mask,ponto1,ponto2,txt_color):
+    x1,y1 = ponto1
+    x2,y2 = ponto2
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    submask = mask[y1:y2,x1:x2]
+
+    pixels = np.sum(submask)/255
+    rgb_mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+
+    return pixels, rgb_mask
+
+def find_extreme_points(image):
+    # load the image, convert it to grayscale, and blur it slightly
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    # threshold the image, then perform a series of erosions +
+    # dilations to remove any small regions of noise
+    thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.erode(thresh, None, iterations=2)
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    # find contours in thresholded image, then grab the largest
+    # one
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+    	cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    c = max(cnts, key=cv2.contourArea)
+
+    # determine the most extreme points along the contour
+    extLeft = tuple(c[c[:, :, 0].argmin()][0])
+    extRight = tuple(c[c[:, :, 0].argmax()][0])
+    extTop = tuple(c[c[:, :, 1].argmin()][0])
+    extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+    # draw the outline of the object, then draw each of the
+    # extreme points, where the left-most is red, right-most
+    # is green, top-most is blue, and bottom-most is teal
+    cv2.drawContours(image, [c], -1, (0, 255, 255), 2)
+    cv2.circle(image, extLeft, 8, (0, 0, 255), -1)
+    cv2.circle(image, extRight, 8, (0, 255, 0), -1)
+    cv2.circle(image, extTop, 8, (255, 0, 0), -1)
+    cv2.circle(image, extBot, 8, (255, 255, 0), -1)
+    # show the output image
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
+
+    return extRight, extBot, extLeft, extTop
 
 
+def find_box_points(mask):
+   
+    contornos, arvore = cv2.findContours(mask.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    for c in contornos:
+        area = cv2.contourArea(c)
+        rect = cv2.minAreaRect(c)
+        box = cv2.boxPoints(rect)
+        # convert all coordinates floating point values to int
+        box = np.int0(box)
+        # draw a green 'nghien' rectangle
+        if area>500:
+            cv2.drawContours(mask, [box], 0, (0, 255, 0),1)
+            print([box])
+
+
+zero = Twist(Vector3(0,0,0), Vector3(0,0,0))
+dire = Twist(Vector3(0,0,0), Vector3(0,0,-w))
+esq = Twist(Vector3(0,0,0), Vector3(0,0,w))
+frente = Twist(Vector3(v,0,0), Vector3(0,0,0))
